39a5cd0ffaac88b245b91fb0b7582f70f30c1ea1
Arturo Gutierrez <arturo.gutierrez@gmail.com>
2016-02-05 11:37:45 +0100

    WIP Account repository impl

---
+++ data/src/main/java/com/arturogutierrez/openticator/data/repository/AccountRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.arturogutierrez.openticator.data.repository;
+
+import com.arturogutierrez.openticator.domain.model.Account;
+import com.arturogutierrez.openticator.domain.repository.AccountRepository;
+import java.util.List;
+import rx.Observable;
+
+public class AccountRepositoryImpl implements AccountRepository {
+
+  @Override
+  public Observable<Account> add(Account account) {
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+
+  @Override
+  public Observable<Account> update(Account account) {
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+
+  @Override
+  public Observable<Account> delete(Account account) {
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+
+  @Override
+  public Observable<List<Account>> getAccounts() {
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+}
