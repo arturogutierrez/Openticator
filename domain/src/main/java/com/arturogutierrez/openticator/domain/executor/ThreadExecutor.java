5aa153c4332593b7b2f253577b5d94f624ec02dc
Arturo Gutierrez <arturo.gutierrez@gmail.com>
2016-02-05 11:37:24 +0100

    Job executor added

---
+++ domain/src/main/java/com/arturogutierrez/openticator/domain/executor/ThreadExecutor.java
@@ -0,0 +1,10 @@
+package com.arturogutierrez.openticator.domain.executor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Executor where all job done by interactors will be executed. Always out of the UI thread.
+ */
+public interface ThreadExecutor extends Executor {
+
+}
