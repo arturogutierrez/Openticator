53bf3cb07d266c189a03dca695c79ebfb594c429
Arturo Gutierrez <arturo.gutierrez@gmail.com>
2016-02-08 16:34:20 +0100

    DiskDataStoreTest fixed

--- data/src/test/java/com/arturogutierrez/openticator/data/ApplicationTestCase.java
+++ data/src/test/java/com/arturogutierrez/openticator/data/ApplicationTestCase.java
@@ -1,10 +1,12 @@
 package com.arturogutierrez.openticator.data;
 
 import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, application = ApplicationStub.class, sdk = 21)
+@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*"})
 public abstract class ApplicationTestCase {
 }
