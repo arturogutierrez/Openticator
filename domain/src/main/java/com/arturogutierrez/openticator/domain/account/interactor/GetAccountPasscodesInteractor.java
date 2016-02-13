a0204abaefcbb9d2fa647c3dd6bb8380c21ba205
Arturo Gutierrez <agd@tid.es>
2016-02-13 16:30:59 +0100

    Creating a new interaction which calculates all passcodes

---
+++ domain/src/main/java/com/arturogutierrez/openticator/domain/account/interactor/GetAccountPasscodesInteractor.java
@@ -0,0 +1,42 @@
+package com.arturogutierrez.openticator.domain.account.interactor;
+
+import com.arturogutierrez.openticator.domain.account.model.Account;
+import com.arturogutierrez.openticator.domain.account.model.AccountPasscode;
+import com.arturogutierrez.openticator.domain.account.repository.AccountRepository;
+import com.arturogutierrez.openticator.domain.otp.OneTimePassword;
+import com.arturogutierrez.openticator.domain.otp.OneTimePasswordFactory;
+import com.arturogutierrez.openticator.executor.PostExecutionThread;
+import com.arturogutierrez.openticator.executor.ThreadExecutor;
+import com.arturogutierrez.openticator.interactor.Interactor;
+import java.util.List;
+import rx.Observable;
+
+public class GetAccountPasscodesInteractor extends Interactor<List<AccountPasscode>> {
+
+  private final AccountRepository accountRepository;
+  private final OneTimePasswordFactory oneTimePasswordFactory;
+
+  public GetAccountPasscodesInteractor(AccountRepository accountRepository,
+      OneTimePasswordFactory oneTimePasswordFactory, ThreadExecutor threadExecutor,
+      PostExecutionThread postExecutionThread) {
+    super(threadExecutor, postExecutionThread);
+    this.accountRepository = accountRepository;
+    this.oneTimePasswordFactory = oneTimePasswordFactory;
+  }
+
+  @Override
+  public Observable<List<AccountPasscode>> createObservable() {
+    return accountRepository.getAccounts()
+        .flatMap(Observable::from)
+        .map(this::calculatePasscode)
+        .toList();
+  }
+
+  private AccountPasscode calculatePasscode(Account account) {
+    // TODO: Pick right delta offset time
+    OneTimePassword oneTimePassword = oneTimePasswordFactory.createOneTimePassword(account, 0);
+    String passcode = oneTimePassword.generate();
+    return new AccountPasscode(account.getName(), account.getIssuer(), passcode,
+        AccountPasscode.INFINITE);
+  }
+}
