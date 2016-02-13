e2f005d997a66833fa276407b660f18fa48c9873
Arturo Gutierrez <arturo.gutierrez@gmail.com>
2016-02-13 17:57:32 +0100

    Adding Next action to master password wizard

--- app/src/main/java/com/arturogutierrez/openticator/domain/password/wizard/view/MasterPasswordFragment.java
+++ app/src/main/java/com/arturogutierrez/openticator/domain/password/wizard/view/MasterPasswordFragment.java
@@ -1,6 +1,12 @@
 package com.arturogutierrez.openticator.domain.password.wizard.view;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import com.arturogutierrez.openticator.R;
+import com.arturogutierrez.openticator.domain.password.wizard.di.MasterPasswordComponent;
 import com.arturogutierrez.openticator.view.fragment.BaseFragment;
 
 public class MasterPasswordFragment extends BaseFragment {
@@ -10,7 +16,51 @@ public class MasterPasswordFragment extends BaseFragment {
   }
 
   @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setHasOptionsMenu(true);
+  }
+
+  @Override
+  public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+
+    initialize();
+  }
+
+  @Override
+  public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+    super.onCreateOptionsMenu(menu, inflater);
+
+    inflater.inflate(R.menu.master_password, menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.action_next:
+        onNextClicked();
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+
+  private void initialize() {
+    initializeInjector();
+  }
+
+  @Override
   protected int getLayoutResource() {
     return R.layout.fragment_master_password;
+  }
+
+  private void initializeInjector() {
+    getComponent(MasterPasswordComponent.class).inject(this);
+  }
+
+  private void onNextClicked() {
+
   }
 }
