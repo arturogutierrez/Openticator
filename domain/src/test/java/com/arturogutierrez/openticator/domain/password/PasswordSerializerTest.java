479cdc3e3c3137bff3f2bed91f2ab9950017d0ad
Arturo Gutierrez <arturo.gutierrez@gmail.com>
2016-02-13 23:29:05 +0100

    Adding CreateMasterPassword interactor

---
+++ domain/src/test/java/com/arturogutierrez/openticator/domain/password/PasswordSerializerTest.java
@@ -0,0 +1,39 @@
+package com.arturogutierrez.openticator.domain.password;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PasswordSerializerTest {
+
+  private PasswordSerializer passwordSerializer;
+
+  @Before
+  public void setUp() {
+    passwordSerializer = new PasswordSerializer();
+  }
+
+  @Test
+  public void testNullPasswordReturnNullSerialization() {
+    String encodedPassword = passwordSerializer.encodePassword(null);
+
+    assertThat(encodedPassword, is(nullValue()));
+  }
+
+  @Test
+  public void testEmptyPasswordReturnSerialization() {
+    String encodedPassword = passwordSerializer.encodePassword("");
+
+    assertThat(encodedPassword, is("47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU="));
+  }
+
+  @Test
+  public void testSerializationWithSHA256() {
+    String encodedPassword = passwordSerializer.encodePassword("password");
+
+    assertThat(encodedPassword, is("XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg="));
+  }
+}
