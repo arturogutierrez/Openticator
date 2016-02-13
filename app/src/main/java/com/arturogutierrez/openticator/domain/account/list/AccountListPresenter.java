f005fa4a4f735bb1790a0b8e9456d4126bd68f18
Arturo Gutierrez <arturo.gutierrez@gmail.com>
2016-02-13 16:30:59 +0100

    Creating a new interaction which calculates all passcodes

--- app/src/main/java/com/arturogutierrez/openticator/domain/account/list/AccountListPresenter.java
+++ app/src/main/java/com/arturogutierrez/openticator/domain/account/list/AccountListPresenter.java
@@ -2,34 +2,27 @@ package com.arturogutierrez.openticator.domain.account.list;
 
 import android.os.Handler;
 import android.os.Looper;
-import com.arturogutierrez.openticator.domain.account.interactor.GetAccountsInteractor;
-import com.arturogutierrez.openticator.domain.account.model.Account;
+import com.arturogutierrez.openticator.domain.account.interactor.GetAccountPasscodesInteractor;
 import com.arturogutierrez.openticator.domain.account.model.AccountPasscode;
-import com.arturogutierrez.openticator.domain.otp.OneTimePassword;
-import com.arturogutierrez.openticator.domain.otp.OneTimePasswordFactory;
 import com.arturogutierrez.openticator.interactor.DefaultSubscriber;
 import com.arturogutierrez.openticator.view.presenter.Presenter;
-import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 
-public class AccountListPresenter extends DefaultSubscriber<List<Account>> implements Presenter {
+public class AccountListPresenter extends DefaultSubscriber<List<AccountPasscode>>
+    implements Presenter {
 
-  private final GetAccountsInteractor getAccountsInteractorInteractor;
-  private final OneTimePasswordFactory oneTimePasswordFactory;
+  private final GetAccountPasscodesInteractor getAccountPasscodesInteractor;
   private AccountListView view;
-  private List<Account> accounts;
   private Handler handler;
   private Runnable scheduleRunnable;
 
   @Inject
-  public AccountListPresenter(GetAccountsInteractor getAccountsInteractorInteractor,
-      OneTimePasswordFactory oneTimePasswordFactory) {
-    this.getAccountsInteractorInteractor = getAccountsInteractorInteractor;
-    this.oneTimePasswordFactory = oneTimePasswordFactory;
+  public AccountListPresenter(GetAccountPasscodesInteractor getAccountPasscodesInteractor) {
+    this.getAccountPasscodesInteractor = getAccountPasscodesInteractor;
     this.handler = new Handler(Looper.getMainLooper());
     this.scheduleRunnable = () -> {
-      updateAndRenderAccountPasscodes();
+      reloadPasscodes();
       scheduleUpdate();
     };
   }
@@ -40,61 +33,34 @@ public class AccountListPresenter extends DefaultSubscriber<List<Account>> imple
 
   @Override
   public void resume() {
-    getAccountsInteractorInteractor.execute(this);
+    getAccountPasscodesInteractor.execute(this);
   }
 
   @Override
   public void pause() {
-    getAccountsInteractorInteractor.unsubscribe();
+    getAccountPasscodesInteractor.unsubscribe();
     cancelSchedule();
   }
 
   @Override
   public void destroy() {
-    getAccountsInteractorInteractor.unsubscribe();
+    getAccountPasscodesInteractor.unsubscribe();
   }
 
   @Override
-  public void onNext(List<Account> accounts) {
-    if (accounts == null || accounts.size() == 0) {
+  public void onNext(List<AccountPasscode> accountPasscodes) {
+    if (accountPasscodes == null || accountPasscodes.size() == 0) {
       view.viewNoItems();
       return;
     }
 
-    saveAccounts(accounts);
-    updateAndRenderAccountPasscodes();
+    view.renderAccounts(accountPasscodes);
     scheduleUpdate();
   }
 
   @Override
   public void onError(Throwable e) {
     view.viewNoItems();
-  }
-
-  private void saveAccounts(List<Account> accounts) {
-    this.accounts = accounts;
-  }
-
-  private void updateAndRenderAccountPasscodes() {
-    List<AccountPasscode> accountPasscodes = calculatePasscodes(accounts);
-    view.renderAccounts(accountPasscodes);
-  }
-
-  private List<AccountPasscode> calculatePasscodes(List<Account> accounts) {
-    List<AccountPasscode> accountPasscodes = new ArrayList<>(accounts.size());
-    for (Account account : accounts) {
-      AccountPasscode accountPasscode = calculatePasscode(account);
-      accountPasscodes.add(accountPasscode);
-    }
-    return accountPasscodes;
-  }
-
-  private AccountPasscode calculatePasscode(Account account) {
-    // TODO: Pick right delta offset time
-    OneTimePassword oneTimePassword = oneTimePasswordFactory.createOneTimePassword(account, 0);
-    String passcode = oneTimePassword.generate();
-    return new AccountPasscode(account.getName(), account.getIssuer(), passcode,
-        AccountPasscode.INFINITE);
   }
 
   private void scheduleUpdate() {
@@ -104,5 +70,10 @@ public class AccountListPresenter extends DefaultSubscriber<List<Account>> imple
 
   private void cancelSchedule() {
     handler.removeCallbacks(scheduleRunnable);
+  }
+
+  private void reloadPasscodes() {
+    getAccountPasscodesInteractor.unsubscribe();
+    getAccountPasscodesInteractor.execute(this);
   }
 }
--- app/src/main/java/com/arturogutierrez/openticator/domain/account/list/di/AccountListModule.java
+++ app/src/main/java/com/arturogutierrez/openticator/domain/account/list/di/AccountListModule.java
@@ -1,8 +1,10 @@
 package com.arturogutierrez.openticator.domain.account.list.di;
 
 import com.arturogutierrez.openticator.di.PerActivity;
+import com.arturogutierrez.openticator.domain.account.interactor.GetAccountPasscodesInteractor;
 import com.arturogutierrez.openticator.domain.account.interactor.GetAccountsInteractor;
 import com.arturogutierrez.openticator.domain.account.repository.AccountRepository;
+import com.arturogutierrez.openticator.domain.otp.OneTimePasswordFactory;
 import com.arturogutierrez.openticator.executor.PostExecutionThread;
 import com.arturogutierrez.openticator.executor.ThreadExecutor;
 import dagger.Module;
@@ -20,5 +22,14 @@ public class AccountListModule {
   GetAccountsInteractor provideGetAccountsInteractor(AccountRepository accountRepository,
       ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread) {
     return new GetAccountsInteractor(accountRepository, threadExecutor, postExecutionThread);
+  }
+
+  @Provides
+  @PerActivity
+  GetAccountPasscodesInteractor provideGetAccountPasscodesInteractor(
+      AccountRepository accountRepository, OneTimePasswordFactory oneTimePasswordFactory,
+      ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread) {
+    return new GetAccountPasscodesInteractor(accountRepository, oneTimePasswordFactory,
+        threadExecutor, postExecutionThread);
   }
 }
