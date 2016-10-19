8b9b4f469227f61a256ceaa1896392af8e230535
Arturo Gutierrez <arturo.gutierrez@gmail.com>
2016-10-19 15:29:15 +0200

    [Kotlin] PasswordSerializerImpl

---
+++ data/src/main/java/com/arturogutierrez/openticator/domain/password/PasswordSerializerImpl.kt
@@ -0,0 +1,33 @@
+package com.arturogutierrez.openticator.domain.password
+
+import android.util.Base64
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import javax.inject.Inject
+
+class PasswordSerializerImpl
+@Inject
+constructor() : PasswordSerializer {
+
+    override fun encodePassword(plainPassword: String): String? {
+        if (plainPassword == null) {
+            return null
+        }
+
+        val messageDigest = createMessageDigest() ?: return null
+
+        messageDigest.update(plainPassword.toByteArray())
+        val digest = messageDigest.digest()
+        return Base64.encodeToString(digest, Base64.NO_WRAP).trim { it <= ' ' }
+    }
+
+    private fun createMessageDigest(): MessageDigest? {
+        try {
+            return MessageDigest.getInstance("SHA-512")
+        } catch (e: NoSuchAlgorithmException) {
+            // Should not happen, all Android devices should support SHA-256
+            return null
+        }
+
+    }
+}
