63e5c003e2a2007c9d6f59ba1f849cda20d2051f
Arturo Gutierrez <arturo.gutierrez@gmail.com>
2016-10-19 10:45:40 +0200

    Last Java tests converted to Kotlin

---
+++ domain/src/test/kotlin/com/arturogutierrez/openticator/domain/category/CategoryFactoryTest.kt
@@ -0,0 +1,25 @@
+package com.arturogutierrez.openticator.domain.category
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class CategoryFactoryTest {
+
+    private lateinit var categoryFactory: CategoryFactory
+
+    @Before
+    fun setUp() {
+        categoryFactory = CategoryFactory()
+    }
+
+    @Test
+    fun testCreatingCategory() {
+        val category = categoryFactory.createCategory("name")
+
+        assertThat(category.categoryId, `is`(notNullValue()))
+        assertThat(category.name, `is`("name"))
+    }
+}
