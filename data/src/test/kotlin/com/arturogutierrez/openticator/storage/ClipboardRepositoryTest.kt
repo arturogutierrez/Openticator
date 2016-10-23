344b829bea1d9d6b8bf8bd4a9d24cff9088bc47a
Arturo Gutierrez <arturo.gutierrez@gmail.com>
2016-10-23 23:20:15 +0200

    Tests added for clipboard

---
+++ data/src/test/kotlin/com/arturogutierrez/openticator/storage/ClipboardRepositoryTest.kt
@@ -0,0 +1,43 @@
+package com.arturogutierrez.openticator.storage
+
+import android.content.ClipDescription
+import android.content.ClipboardManager
+import android.content.Context
+import com.arturogutierrez.openticator.ApplicationTestCase
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.robolectric.RuntimeEnvironment
+
+class ClipboardRepositoryTest : ApplicationTestCase() {
+
+  private lateinit var clipboardRepository: ClipboardRepositoryImpl
+  private lateinit var clipboardManager: ClipboardManager
+
+  companion object {
+    val fakeLabel = "LABEL"
+    val fakeText = "TEXT"
+  }
+
+  @Before
+  fun setUp() {
+    val context = RuntimeEnvironment.application
+    clipboardRepository = ClipboardRepositoryImpl(context)
+    clipboardManager = RuntimeEnvironment.application.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+  }
+
+  @Test
+  fun testTextIsCopiedToClipboard() {
+    clipboardRepository.copy(fakeLabel, fakeText)
+
+    val isPlainMimeType = clipboardManager.primaryClipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
+    val label = clipboardManager.primaryClipDescription.label.toString()
+    val clipData = clipboardManager.primaryClip.getItemAt(0)
+    val text = clipData.text.toString()
+
+    assertThat(isPlainMimeType, `is`(true))
+    assertThat(label, `is`(fakeLabel))
+    assertThat(text, `is`(fakeText))
+  }
+}
