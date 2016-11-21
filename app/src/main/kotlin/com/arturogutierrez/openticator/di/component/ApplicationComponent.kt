ea212c7e2b9caa440fcec8b2336d944df55d3865
Arturo Gutiérrez <arturo.gutierrez@gmail.com>
2016-11-21 17:03:07 +0100

    Configuring Realm in Application always (#38)

--- app/src/main/kotlin/com/arturogutierrez/openticator/di/component/ApplicationComponent.kt
+++ app/src/main/kotlin/com/arturogutierrez/openticator/di/component/ApplicationComponent.kt
@@ -3,9 +3,8 @@ package com.arturogutierrez.openticator.di.component
 import android.content.Context
 import android.content.SharedPreferences
 import android.view.LayoutInflater
+import com.arturogutierrez.openticator.application.OpenticatorApplication
 import com.arturogutierrez.openticator.di.module.ApplicationModule
-import com.arturogutierrez.openticator.storage.database.DatabaseConfigurator
-import com.arturogutierrez.openticator.storage.preferences.Preferences
 import com.arturogutierrez.openticator.domain.account.repository.AccountRepository
 import com.arturogutierrez.openticator.domain.category.CategorySelector
 import com.arturogutierrez.openticator.domain.category.repository.CategoryRepository
@@ -13,6 +12,8 @@ import com.arturogutierrez.openticator.domain.issuer.repository.IssuerRepository
 import com.arturogutierrez.openticator.executor.PostExecutionThread
 import com.arturogutierrez.openticator.executor.ThreadExecutor
 import com.arturogutierrez.openticator.storage.clipboard.ClipboardRepository
+import com.arturogutierrez.openticator.storage.database.DatabaseConfigurator
+import com.arturogutierrez.openticator.storage.preferences.Preferences
 import com.arturogutierrez.openticator.view.activity.BaseActivity
 import com.arturogutierrez.openticator.view.activity.ProxyActivity
 import dagger.Component
@@ -21,6 +22,9 @@ import javax.inject.Singleton
 @Singleton
 @Component(modules = arrayOf(ApplicationModule::class))
 interface ApplicationComponent {
+
+  fun inject(application: OpenticatorApplication)
+
   fun inject(proxyActivity: ProxyActivity)
 
   fun inject(baseActivity: BaseActivity)
