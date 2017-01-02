a39228cb9a0b0b0ff043d44e284d1d66ff07c8c0
Arturo Gutiérrez <arturo.gutierrez@gmail.com>
2017-01-02 17:53:39 +0100

    Passcode animations (#66)
    * Using Robinhood Ticker to render the passcodes

--- app/src/main/kotlin/com/arturogutierrez/openticator/domain/account/list/adapter/AccountViewHolder.kt
+++ app/src/main/kotlin/com/arturogutierrez/openticator/domain/account/list/adapter/AccountViewHolder.kt
@@ -2,6 +2,7 @@ package com.arturogutierrez.openticator.domain.account.list.adapter
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.view.animation.OvershootInterpolator
 import android.widget.ImageView
 import android.widget.TextView
 import com.arturogutierrez.openticator.R
@@ -10,24 +11,24 @@ import com.arturogutierrez.openticator.domain.account.model.AccountPasscode
 import com.arturogutierrez.openticator.domain.issuer.IssuerDecoratorFactory
 import com.arturogutierrez.openticator.domain.otp.time.CurrentTimeProvider
 import com.arturogutierrez.openticator.domain.otp.time.RemainingTimeCalculator
+import com.robinhood.ticker.TickerUtils
+import com.robinhood.ticker.TickerView
 import org.jetbrains.anko.find
 import org.jetbrains.anko.imageResource
 
 class AccountViewHolder(itemView: View, onItemClick: (position: Int) -> Unit,
                         onLongItemClick: (Int) -> Boolean) : RecyclerView.ViewHolder(itemView) {
 
-  private val issuerDecoratorFactory: IssuerDecoratorFactory
-  private val ivIcon: ImageView
-  private val tvName: TextView
-  private val tvPasscode: TextView
-  private val vCountdown: CountdownWidget
+  private val issuerDecoratorFactory = IssuerDecoratorFactory(itemView.context)
+  private val ivIcon = itemView.find<ImageView>(R.id.iv_icon)
+  private val tvName = itemView.find<TextView>(R.id.tv_account_name)
+  private val tvPasscode = itemView.find<TickerView>(R.id.tv_code)
+  private val vCountdown = itemView.find<CountdownWidget>(R.id.v_countdown)
 
   init {
-    issuerDecoratorFactory = IssuerDecoratorFactory(itemView.context)
-    ivIcon = itemView.find<ImageView>(R.id.iv_icon)
-    tvName = itemView.find<TextView>(R.id.tv_account_name)
-    tvPasscode = itemView.find<TextView>(R.id.tv_code)
-    vCountdown = itemView.find<CountdownWidget>(R.id.v_countdown)
+    tvPasscode.setCharacterList(TickerUtils.getDefaultNumberList())
+    tvPasscode.animationDuration = 800
+    tvPasscode.animationInterpolator = OvershootInterpolator(1.4f)
 
     itemView.setOnClickListener { onItemClick(layoutPosition) }
     itemView.setOnLongClickListener { onLongItemClick(layoutPosition) }
@@ -37,7 +38,7 @@ class AccountViewHolder(itemView: View, onItemClick: (position: Int) -> Unit,
     val issuerDecorator = issuerDecoratorFactory.create(accountPasscode.issuer)
     ivIcon.imageResource = issuerDecorator.imageResource
     tvName.text = accountPasscode.account.name
-    tvPasscode.text = accountPasscode.passcode.code
+    tvPasscode.setText(accountPasscode.passcode.code)
     itemView.isSelected = isSelected
 
     val animationLength = calculateRemainingSeconds(accountPasscode.passcode.validUntil)
