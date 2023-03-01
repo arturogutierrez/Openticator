8fecb65d613a43d6e05ac7d160fccb00ad10ef15
Miguel Barrios <miguel.benito@goodnotesapp.com>
2023-03-01 22:51:03 +0100

    #86 Make isBase32 function case insensitive

--- domain/src/main/kotlin/com/arturogutierrez/openticator/helpers/StringExt.kt
+++ domain/src/main/kotlin/com/arturogutierrez/openticator/helpers/StringExt.kt
@@ -3,6 +3,6 @@ package com.arturogutierrez.openticator.helpers
 import java.util.regex.Pattern
 
 fun String.isBase32(): Boolean {
-  val pattern = Pattern.compile("[a-z2-7]+")
-  return pattern.matcher(toUpperCase()).matches()
+  val pattern = Pattern.compile("[A-Za-z2-7]+")
+  return pattern.matcher(this).matches()
 }
